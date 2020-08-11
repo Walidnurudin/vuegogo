68f22e753cfd7b5aa093c096cef5c79e0065a1f6
Walid nurudin <walidnurudin47@gmail.com>
2020-08-11 16:57:25 +0700

    first commit

---
+++ src/views/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="profile">
+        <h1>This is an Profile page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Profile'
+}
+</script>
+
+<style>
+
+</style>
