58affd38f139cafc406b8f5bdad10578f5323cf8
Walid nurudin <walidnurudin47@gmail.com>
2020-09-12 21:52:01 +0700

    add firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Local from '../views/Local.vue'
 import Profile from '../views/Profile.vue'
 import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/local',
+    name: 'Local',
+    component: Local
   },
   {
     path: '/profile',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,90 +1,15 @@
 <template>
-  <b-container>
-    <h1>To Do List</h1>
-
-    <b-row class="justify-content-center">
-      <b-col cols="8">
-        <b-form inline class="justify-content-center">
-          <!-- v-model = untuk menampung value -->
-          <b-input
-            type="text"
-            v-model="todo"
-            v-on:keypress.enter.prevent="addTodo"
-            placeholder="To do..."
-          ></b-input>
-          <b-button class="ml-3" variant="success" v-on:click="addTodo()">Add</b-button>
-        </b-form>
-      </b-col>
-    </b-row>
-
-    <!-- List -->
-    <b-row class="justify-content-center mt-5">
-      <b-col cols="8">
-        <table class="table">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Todo</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="todos.length > 0">
-            <tr v-for="(item, key) in todos" v-bind:key="key">
-              <td>{{key + 1}}</td>
-              <td>
-                <div v-if="show !== key">{{item}}</div>
-                <b-input v-if="show === key" type="text" v-model="update" @keypress.enter="save(key)" placeholder="update..."></b-input>
-              </td>
-              <td>
-                <b-button class="mr-3" variant="danger" v-on:click="deleteTodo(key)">delete</b-button>
-                <b-button v-if="show !== key" variant="outline-primary" @click="up(item, key)">update</b-button>
-                <b-button v-if="show === key" variant="primary" @click="save(key)">save</b-button>
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-else>
-            <tr>
-              <td>None</td>
-              <td>None</td>
-              <td>None</td>
-            </tr>
-          </tbody>
-        </table>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+      Home
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  data() {
-    return {
-      todo: "",
-      todos: [],
-      show: false,
-      update: ""
-    };
-  },
-  methods: {
-    addTodo() {
-      this.todos.push(this.todo);
-      this.todo = "";
-    },
-    deleteTodo(index) {
-      this.todos.splice(index, 1);
-    },
-    up(i, key) {
-      this.show = key;
-      this.update = i;
-    },
-    save(id) {
-      this.show = true;
-      this.todos.splice(id, 1, this.update);
-    }
-  }
-};
+    name: 'Home'
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+
 </style>
